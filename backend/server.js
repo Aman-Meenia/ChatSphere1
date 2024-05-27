a9ba97c46f1e6feda64409e85476a8c31769e21d
Aman-Meenia <amanmeenia09876@gmail.com>
2024-05-27 14:49:32 +0530

    add render origin

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://chat-sphere1.vercel.app", "http://localhost:3001"],
     credentials: true,
     withCredentials: true,
   }),
